f90fbc206c05f29f6e8e81d2a5d5d731abaf9c04
Pedro Leal <pedrombl@gmail.com>
2014-03-12 20:10:35 -0300

    Removing hard code for parsing the dice

--- lib.js
+++ lib.js
@@ -1,3 +1,4 @@
-function generate() {
-  return [1,6];
+function generate(dice) {
+  var parts = dice.split('d');
+  return [parseInt(parts[0]), parseInt(parts[1])];
 };
--- spec.js
+++ spec.js
@@ -8,5 +8,9 @@ describe('range generation', function () {
   it('gives the range within min and max values', function () {
     expect(generate('1d6')).toEqual([1,6]);
   });
+
+  it('gives the range within min and max for 1d10', function () {
+    expect(generate('1d10')).toEqual([1,10]);
+  });
 });
 
